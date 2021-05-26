f59866fc0b5c5f96d43b801150b32a88e669b361
CY <chendaxians@outlook.com>
2021-05-26 16:20:10 +0800

    添加后天多线程心跳包

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import socket  # 导入 socket 模块
 from flask import Flask
 from flask import render_template
-from flask import url_for
 from flask import request
 import time
+import threading
 
 
 def send_to_open():
@@ -72,10 +72,49 @@ def open_door_test():
         qos = 1
 
 
+class myThread(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        print("开始线程：" + self.name)
+        test_connect()
+        print("退出线程：" + self.name)
+
+
+def test_connect():
+    global c
+    global qos
+    a1 = "01 0F 00 00 00 04 01 00 3E 96"
+
+    while True:
+        if qos == 1:
+            try:
+                print('心跳包')
+                c.send(bytes.fromhex(a1))
+                time.sleep(0.5)
+                c.send(bytes.fromhex(a1))
+                time.sleep(0.5)
+                c.send(bytes.fromhex(a1))
+                time.sleep(0.5)
+                c.send(bytes.fromhex(a1))
+                time.sleep(0.5)
+                time.sleep(600)
+            except:
+                print('关闭了正在占线的链接！')
+                kill_socket()
+                print('关闭实例')
+
+                c = connect()
+                print('新建实例')
+
+
 app = Flask(__name__)
 c = connect()
 qos = 1
-User_admin = ['qqbot', '任意添加']
+User_admin = ['qqbot', '1007800006', '2200475850', '1821269010', '1005944615', '1038888008', 'hardware']
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -87,5 +126,6 @@ def index():
         return render_template('bad.html')
 
 
-if __name__ == "__main__":
-    app.run(port=14725, host='0.0.0.0')
+thread1 = myThread(1, "Thread-1")
+thread1.start()
+app.run(port=14725, host='0.0.0.0')
